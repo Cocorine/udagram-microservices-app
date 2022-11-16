e11684332b2feca1408e07100c7ce35b13236484
marieloujo <detchenoujoan@gmail.com>
2022-11-16 21:57:30 +0100

    Update backend deployement url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aef044ef5cd2942319b46569c7c23ade-2008166838.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
